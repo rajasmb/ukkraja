34a26cd2061571a051520dc4371358a2ade13178
rajasmb <rajasembiring365@gmail.com>
2024-02-28 11:39:48 +0700

    first commit

---
+++ admin/layout/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="icon" href="../asset/img/subtractt.png">
+  </head>
+  <body>
+    <div class="container">
+      <h3 class="my-4">PERPUS</h3>
+      <div class="card" style="background-color: #365486;">
+        <div class="card-body ms-auto">
+          <a href="index_adm.php" class="btn text-white">Dashboard</a>
+          <a href="anggota.php" class="btn text-white">Anggota</a>
+          <a href="buku.php" class="btn text-white">Data Buku</a>
+          <a href="peminjam.php" class="btn text-white">Data Peminjam</a>
+          <a href="laporan.php" class="btn text-white">Laporan</a>
+          <a href="../logout.php" class="btn text-white">Logout</a>
+        </div>
+      </div>
+    </div>
---
+++ peminjam/layout/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Peminjam</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="icon" href="../asset/img/subtractt.png">
+  </head>
+  <body>
+    <div class="container">
+      <h3 class="my-4">PERPUSTAKAAN DIGITAL</h3>
+      <div class="card" style="background-color: #365486;">
+        <div class="card-body">
+          <a href="index_pmj.php" class="btn text-white">Dashboard</a>
+          <a href="buku.php" class="btn text-white">Buku</a>
+          <a href="../logout.php" class="btn text-white">Logout</a>
+        </div>
+      </div>
+    </div>
